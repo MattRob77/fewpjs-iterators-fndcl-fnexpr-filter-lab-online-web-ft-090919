9053faf4ca46e10e187a945e17af06be54276400
Matthew Robinson <matt_robinson1@yahoo.com>
2019-12-26 19:01:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ function findMatching(drivers, toMatch) {
 function fuzzyMatch(drivers, toMatch) { 
   return drivers.filter(match => match.toLowerCase().indexOf(toMatch.toLowerCase()) === 0)
 } 
+
+
+function matchName(drivers, toMatch) { 
+  
+  
+  
+} 
